5d895f14b510899b116df77e1363434e865753cf
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-03-18 11:02:58 +0300

    تكملة تحديث الدرس السابع

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const port = 4000;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
 
-const dataTestProject1 = require("./models/myScema")
+const dataTestProject1 = require("./models/myScema");
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 //
 const path = require("path");
 const livereload = require("livereload");
 const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, 'public'));
- 
- 
+liveReloadServer.watch(path.join(__dirname, "public"));
+
 const connectLivereload = require("connect-livereload");
 app.use(connectLivereload());
- 
+
 liveReloadServer.server.once("connection", () => {
   setTimeout(() => {
     liveReloadServer.refresh("/");
@@ -28,37 +26,29 @@ liveReloadServer.server.once("connection", () => {
 //
 
 //عرض الصفحة المطلوبة عند الذهاب للمسار المطلوب
-app.get('/', (req, res) => {
-  res.render("index")
-  
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/user/add.html', (req,res) => {
-  res.render('/user/add.ejs')
-  
-})
-app.get('/user/view.html', (req,res) => {
-  res.render('/user/view.ejs')
-  
-})
-app.get('/user/edit.html', (req,res) => {
-  res.render('/user/edit.ejs')
-  
-})
-
-
-
-
+app.get("/user/add.html", (req, res) => {
+  res.render("/user/add.ejs");
+});
+app.get("/user/view.html", (req, res) => {
+  res.render("/user/view.ejs");
+});
+app.get("/user/edit.html", (req, res) => {
+  res.render("/user/edit.ejs");
+});
 
 mongoose
-.connect("mongodb+srv://basil:_%23X8p%21_4U%23dmMN8@cluster0.8gkpu.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {
-  app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
+  .connect(
+    "mongodb+srv://basil:_%23X8p%21_4U%23dmMN8@cluster0.8gkpu.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
- })
- .catch((err) => {
-   console.log(err);
- });
- 
- 
